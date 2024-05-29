10116d0f61d4599b1961a62d21fe2d63f01d2fbb
Ivan Vergés <ivan@pokecode.net>
2024-05-29 11:41:47 +0200

    init app

---
+++ db/migrate/20240522096353_add_code_to_decidim_meetings_registrations.decidim_meetings.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# This migration comes from decidim_meetings (originally 20180615160839)
+
+class AddCodeToDecidimMeetingsRegistrations < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_meetings_registrations, :code, :string
+    add_index :decidim_meetings_registrations, :code
+  end
+end
